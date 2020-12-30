d678f35dbab8d60e40c46ab71e8d1c8e2572a500
nitya <nityabhaskar21@gmail.com>
2020-12-30 22:02:58 +0530

    implemented listings

--- src/app/viewallusers/viewallusers.component.ts
+++ src/app/viewallusers/viewallusers.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../user';
 import { UsernService } from '../usern.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-viewallusers',
